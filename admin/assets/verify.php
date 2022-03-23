7af3a8c93f192866a80994e8c262cf21639a563b
Kabelo Mighty Hlungwani <53508373+kabelo-mighty@users.noreply.github.com>
2022-03-24 00:34:43 +0200

    Add files via upload

---
+++ admin/assets/verify.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Authification code</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alata">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/fonts/line-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/Google-Style-Login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body style="background: url(&quot;assets/img/pad.png&quot;);">
+    <div class="login-card" style="border-radius: 0px;border: 0.8px solid var(--yellow);background: url(&quot;assets/img/pad.png&quot;);font-family: Alata, sans-serif;">
+        <p style="text-align: center;"><i class="icon ion-lock-combination" style="color: var(--yellow);font-size: 70px;"></i></p>
+        <p class="profile-name-card" style="color: rgb(255,255,255);font-family: Alata, sans-serif;font-weight: 400;font-size: 30px;">Verify&nbsp;</p>
+        <form class="form-signin"><span class="reauth-email"> </span>
+            <p class="text-center flash animated infinite" style="color: var(--yellow);">Kabelo***@gmail.com</p>
+            <input class="form-control" type="text" id="opt" name="opt" required="" placeholder="OTP code" autofocus="" style="background: url(&quot;assets/img/pad.png&quot;);border-color: var(--yellow);color: var(--yellow);">
+    </div><button class="btn btn-primary btn-block btn-lg btn-signin" type="submit" style="background: url(&quot;assets/img/pad.png&quot;), var(--yellow);font-weight: 400;border: 0.8px solid var(--yellow) ;">Proceed</button>
+        </form><a class="forgot-password" href="index.php" style="border-color: rgb(255,255,255);color: rgb(255,255,255);">Back To Login</a>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
